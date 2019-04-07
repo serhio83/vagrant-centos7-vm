25f52826208b050d7f5a3f2552a08e3314aee137
Sergey Gerasimov <s.gerasimov@fbs.com>
2019-04-07 14:09:17 +0300

    fix

--- Vagrantfile
+++ Vagrantfile
@@ -5,7 +5,7 @@ Vagrant.configure("2") do |config|
   config.vm.box = "centos/7"
   config.vm.network "private_network", ip: "10.10.10.2"
   config.vm.network "forwarded_port", guest: 80, host: 8080
-  config.vm.hostname = "vagrant1"
+  config.vm.hostname = "vm1"
 
   config.vm.provider "virtualbox" do |v|
     # set default nic type for mac os
--- refresh_vm.sh
+++ refresh_vm.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-HOSTNAME='10.10.10.2'
-
 printf "destroying vagrant VM...\n"
 sh -c 'vagrant destroy -f'
-ssh-keygen -R $HOSTNAME
+ssh-keygen -R vm1
 rm -rf $HOME/.ssh/known_hosts.old
 printf "creating vagrant VM...\n"
 sh -c 'vagrant up'
-ssh-keyscan -t ecdsa-sha2-nistp256 $HOSTNAME >> $HOME/.ssh/known_hosts
+ssh-keyscan -t ecdsa-sha2-nistp256 'vm1,10.10.10.2' >> $HOME/.ssh/known_hosts
